710eae30610d7722c51539aa2877bce4aaa944c8
Abraham Martinez Wong <amartinezw@MacBook-Pro-de-ADMIN.local>
2024-08-29 17:20:02 -0600

    Revert "landing con tailwind y mix"
    This reverts commit 8420a70ae52d1ffd3bdccb9e73f3ddc626abe94b.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('landing');
+    return view('welcome');
 });
-
-Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
