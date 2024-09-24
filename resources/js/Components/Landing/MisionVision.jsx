d42462ffb7037f46739d2057ad6e73fd67005061
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2024-09-24 15:27:41 -0600

    fixes a slide de mision

--- resources/js/Components/Landing/MisionVision.jsx
+++ resources/js/Components/Landing/MisionVision.jsx
@@ -8,38 +8,35 @@ export default function MisionVision({ landingData }) {
         <Carousel autoplay={true} loop={true} className="rounded-xl relative">
           {/* Slide 0 - Nosotros */}
           <div className="relative h-full w-full flex items-center flex-col-reverse md:flex-row">
-            {/* Imagen con desvanecimiento (se oculta en dispositivos pequeños) */}
-            <div className="hidden md:block w-1/2 h-full relative ">
+            <div className="hidden md:block w-1/2 h-full relative">
               <img
                 src={`/storage/images/atp-vision.jpg`}
                 alt="image"
-                className="h-full w-full object-cover rounded-lg bg-white/20 shadow-lg ring-1 ring-black/5"
-                style={{
-                  position: 'relative',
-                  zIndex: 1,
-                }}
+                className="h-full w-full object-cover rounded-xl"
+              />
+              {/* Overlay para aplicar el efecto blur */}
+              <div
+                className="absolute inset-0 rounded-xl bg-gradient-to-r from-transparent via-white/50 to-white/70 backdrop-blur-sm"
               />
             </div>
 
             {/* Texto centrado en dispositivos pequeños */}
-            <div className="w-full md:w-1/2 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-10">
-              <div className="relative">
-                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div> {/* Fondo blanco semitransparente */}
-                <Typography
-                  variant="h1"
-                  color="black"
-                  className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
-                >
-                  Nosotros
-                </Typography>
-                <Typography
-                  variant="lead"
-                  color="black"
-                  className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
-                >
-                  {landingData?.section3_vision ?? 'Ser la firma de contabilidad de confianza para todos nuestros clientes, brindando soluciones de alta calidad y accesibles.'}
-                </Typography>
-              </div>
+            <div className="w-full md:w-2/3 lg:w-3/4 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-20 md:-ml-13 lg:-ml-28">
+              <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div> {/* Fondo blanco semitransparente */}
+              <Typography
+                variant="h1"
+                color="black"
+                className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
+              >
+                Nosotros
+              </Typography>
+              <Typography
+                variant="lead"
+                color="black"
+                className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
+              >
+                {landingData?.section3_vision ?? 'Ser la firma de contabilidad de confianza para todos nuestros clientes, brindando soluciones de alta calidad y accesibles.'}
+              </Typography>
             </div>
           </div>
 
@@ -52,27 +49,27 @@ export default function MisionVision({ landingData }) {
                 className="h-full w-full object-cover rounded-xl"
               />
               {/* Overlay con gradiente de desvanecimiento */}
-              <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to-transparent opacity-70"></div>
+              <div
+                className="absolute inset-0 rounded-xl bg-gradient-to-r from-transparent via-white/50 to-white/70 backdrop-blur-sm"
+              />
             </div>
 
-            <div className="w-full md:w-1/2 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-10">
-              <div className="relative">
-                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div>
-                <Typography
-                  variant="h1"
-                  color="black"
-                  className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
-                >
-                  Misión
-                </Typography>
-                <Typography
-                  variant="lead"
-                  color="black"
-                  className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
-                >
-                  {landingData?.section3_mission ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
-                </Typography>
-              </div>
+            <div className="w-full md:w-2/3 lg:w-3/4 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-20 md:-ml-13 lg:-ml-28">
+              <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div>
+              <Typography
+                variant="h1"
+                color="black"
+                className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
+              >
+                Misión
+              </Typography>
+              <Typography
+                variant="lead"
+                color="black"
+                className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
+              >
+                {landingData?.section3_mission ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+              </Typography>
             </div>
           </div>
 
@@ -85,27 +82,26 @@ export default function MisionVision({ landingData }) {
                 className="h-full w-full object-cover rounded-xl"
                 style={{ zIndex: 1 }}
               />
+              <div
+                className="absolute inset-0 rounded-xl bg-gradient-to-r from-transparent via-white/50 to-white/70 backdrop-blur-sm"
+              />
             </div>
-
-
-            <div className="w-full md:w-1/2 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-10">
-              <div className="relative">
-                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div>
-                <Typography
-                  variant="h1"
-                  color="black"
-                  className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
-                >
-                  Visión
-                </Typography>
-                <Typography
-                  variant="lead"
-                  color="black"
-                  className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
-                >
-                  {landingData?.section3_vision ?? 'Ser la firma de contabilidad de confianza para todos nuestros clientes, brindando soluciones de alta calidad y accesibles.'}
-                </Typography>
-              </div>
+            <div className="w-full md:w-2/3 lg:w-3/4 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-20 md:-ml-13 lg:-ml-28">
+              <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div>
+              <Typography
+                variant="h1"
+                color="black"
+                className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
+              >
+                Visión
+              </Typography>
+              <Typography
+                variant="lead"
+                color="black"
+                className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
+              >
+                {landingData?.section3_vision ?? 'Ser la firma de contabilidad de confianza para todos nuestros clientes, brindando soluciones de alta calidad y accesibles.'}
+              </Typography>
             </div>
           </div>
         </Carousel>
