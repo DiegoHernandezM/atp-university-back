fb7f19d7d82e909d4a3910d8d74baf14e595e81d
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2024-09-24 14:46:50 -0600

    cambios a metricas

--- resources/js/Components/Landing/Metricts.jsx
+++ resources/js/Components/Landing/Metricts.jsx
@@ -8,13 +8,7 @@ export default function Metricts({ landingData, containerVariants, countVariants
     <section id="statistics" className="services-section py-20 bg-gradient-to-b from-[#EEEEEE] to-[#BDBDBD]">
       <div className="container mx-auto px-4 text-center">
         <h2 className="text-4xl font-bold mb-12">NUESTRAS ESTADÍSTICAS</h2>
-        <motion.div
-          variants={containerVariants}
-          initial="hidden"
-          whileInView="visible"
-          transition={{ duration: 1.5 }}
-          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8"
-        >
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {landingData.section2_counts ? JSON.parse(landingData.section2_counts).map((count, index) => (
             <motion.div
               variants={countVariants}
@@ -23,10 +17,10 @@ export default function Metricts({ landingData, containerVariants, countVariants
             >
               {/* Flip clock effect for the number */}
 
-                <h1 className="text-5xl font-bold text-white">
-                  {/* Using Flip to display the number */}
-                  <Flip value={count.quantity.toString()} /> {/* Pass the count quantity as the value */}
-                </h1>
+              <h1 className="text-5xl font-bold text-white">
+                {/* Using Flip to display the number */}
+                <Flip value={count.quantity.toString()} /> {/* Pass the count quantity as the value */}
+              </h1>
 
               {/* Title and description */}
               <div className="mt-4">
@@ -35,7 +29,7 @@ export default function Metricts({ landingData, containerVariants, countVariants
               </div>
             </motion.div>
           )) : null}
-        </motion.div>
+        </div>
       </div>
     </section>
   );
--- resources/js/Components/Landing/MisionVision.jsx
+++ resources/js/Components/Landing/MisionVision.jsx
@@ -1,105 +1,107 @@
 import React from "react";
-import { Carousel, Typography, IconButton } from '@material-tailwind/react';
+import { Carousel, Typography } from '@material-tailwind/react';
 
 export default function MisionVision({ landingData }) {
   return (
     <section id="mission-vision" className="statistics-section py-10 bg-transparent relative overflow-hidden">
       <div className="container mx-auto px-4">
-        <Carousel
-          autoplay={true}
-          loop={true}
-          className="rounded-xl relative"
-        >
+        <Carousel autoplay={true} loop={true} className="rounded-xl relative">
           {/* Slide 0 - Nosotros */}
           <div className="relative h-full w-full flex items-center flex-col-reverse md:flex-row">
-            {/* Imagen (se oculta en dispositivos pequeños) */}
-            <div className="hidden md:block w-1/2 h-full">
+            {/* Imagen con desvanecimiento (se oculta en dispositivos pequeños) */}
+            <div className="hidden md:block w-1/2 h-full relative ">
               <img
                 src={`/storage/images/atp-vision.jpg`}
-                alt="image 2"
-                className="h-full w-full object-cover rounded-l-lg"
+                alt="image"
+                className="h-full w-full object-cover rounded-lg bg-white/20 shadow-lg ring-1 ring-black/5"
+                style={{
+                  position: 'relative',
+                  zIndex: 1,
+                }}
               />
             </div>
 
             {/* Texto centrado en dispositivos pequeños */}
             <div className="w-full md:w-1/2 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-10">
               <div className="relative">
-                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full"></div> {/* Fondo blanco semitransparente */}
+                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div> {/* Fondo blanco semitransparente */}
                 <Typography
                   variant="h1"
                   color="black"
-                  className="text-2xl md:text-5xl lg:text-6xl font-bold mb-4 relative z-10"
+                  className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
                 >
                   Nosotros
                 </Typography>
                 <Typography
                   variant="lead"
                   color="black"
-                  className="opacity-80 leading-relaxed text-sm md:text-lg relative z-10"
+                  className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
                 >
                   {landingData?.section3_vision ?? 'Ser la firma de contabilidad de confianza para todos nuestros clientes, brindando soluciones de alta calidad y accesibles.'}
                 </Typography>
               </div>
             </div>
           </div>
-          {/* Slide 1 - Misión */}
+
+          {/* Repetimos el mismo patrón para Misión y Visión */}
           <div className="relative h-full w-full flex items-center flex-col-reverse md:flex-row">
-            {/* Imagen (se oculta en dispositivos pequeños) */}
-            <div className="hidden md:block w-1/2 h-full">
+            <div className="hidden md:block w-1/2 h-full relative ">
               <img
                 src={`/storage/images/atp-vision.jpg`}
                 alt="image 1"
-                className="h-full w-full object-cover rounded-l-lg"
+                className="h-full w-full object-cover rounded-xl"
               />
+              {/* Overlay con gradiente de desvanecimiento */}
+              <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to-transparent opacity-70"></div>
             </div>
 
-            {/* Texto centrado en dispositivos pequeños */}
             <div className="w-full md:w-1/2 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-10">
               <div className="relative">
-                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full"></div> {/* Fondo blanco semitransparente */}
+                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div>
                 <Typography
                   variant="h1"
                   color="black"
-                  className="text-2xl md:text-5xl lg:text-6xl font-bold mb-4 relative z-10"
+                  className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
                 >
                   Misión
                 </Typography>
                 <Typography
                   variant="lead"
                   color="black"
-                  className="opacity-80 leading-relaxed text-sm md:text-lg relative z-10"
+                  className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
                 >
                   {landingData?.section3_mission ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
                 </Typography>
               </div>
             </div>
           </div>
-          {/* Slide 2 - Visión */}
+
           <div className="relative h-full w-full flex items-center flex-col-reverse md:flex-row">
-            {/* Imagen (se oculta en dispositivos pequeños) */}
-            <div className="hidden md:block w-1/2 h-full">
+            <div className="hidden md:block w-1/2 h-full relative ">
+              {/* Imagen */}
               <img
                 src={`/storage/images/atp-vision.jpg`}
                 alt="image 2"
-                className="h-full w-full object-cover rounded-l-lg"
+                className="h-full w-full object-cover rounded-xl"
+                style={{ zIndex: 1 }}
               />
             </div>
 
-            {/* Texto centrado en dispositivos pequeños */}
+
             <div className="w-full md:w-1/2 p-8 md:p-12 lg:p-16 text-center md:text-left bg-transparent relative z-10">
               <div className="relative">
-                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full"></div> {/* Fondo blanco semitransparente */}
+                <div className="md:hidden w-full bg-white opacity-50 absolute inset-0 h-full rounded-lg"></div>
                 <Typography
                   variant="h1"
                   color="black"
-                  className="text-2xl md:text-5xl lg:text-6xl font-bold mb-4 relative z-10"
+                  className="opacity-80 sm:text-4xl lg:text-5xl xl:text-6xl relative z-10"
                 >
                   Visión
                 </Typography>
                 <Typography
                   variant="lead"
                   color="black"
-                  className="opacity-80 leading-relaxed text-sm md:text-lg relative z-10"
+                  className="opacity-80 sm:text-4xl lg:text-3xl xl:text-3xl relative z-10"
                 >
                   {landingData?.section3_vision ?? 'Ser la firma de contabilidad de confianza para todos nuestros clientes, brindando soluciones de alta calidad y accesibles.'}
                 </Typography>
@@ -107,31 +109,6 @@ export default function MisionVision({ landingData }) {
             </div>
           </div>
         </Carousel>
-
-        {/* Botones de navegación personalizados */}
-        <IconButton
-          variant="text"
-          color="white"
-          size="lg"
-          className="!absolute top-2/4 left-4 z-20 -translate-y-2/4"
-          onClick={() => document.querySelector('.carousel').prev()}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="h-6 w-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-          </svg>
-        </IconButton>
-
-        <IconButton
-          variant="text"
-          color="white"
-          size="lg"
-          className="!absolute top-2/4 right-4 z-20 -translate-y-2/4"
-          onClick={() => document.querySelector('.carousel').next()}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="h-6 w-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-          </svg>
-        </IconButton>
       </div>
     </section>
   );
