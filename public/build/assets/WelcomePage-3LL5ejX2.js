7d29da3f225763e80c017c8feb32673b43a19928
Abraham Martinez Wong <amartinezw@MacBook-Pro-de-ADMIN.local>
2024-10-31 15:09:27 -0600

    rebuild

--- public/build/assets/WelcomePage-3LL5ejX2.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-D8ykFVA0.js";import{N as s,P as a}from"./PrincipalVideo-CUDSjbIQ.js";import"./proxy-DL30lgEo.js";function p({landingData:i,fixedNav:r=!0,isPrev:t}){return e.jsxs("div",{className:"landing-page",children:[e.jsx(s,{title:i.title,fixed:r}),e.jsx(a,{landingData:i,isPrev:t})]})}export{p as default};
---
+++ public/build/assets/WelcomePage-DF50pJL0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-DI_yhX2z.js";import{N as s,P as a}from"./PrincipalVideo-BiKOWPz3.js";import"./proxy-CqUGUJPo.js";function p({landingData:i,fixedNav:r=!0,isPrev:t}){return e.jsxs("div",{className:"landing-page",children:[e.jsx(s,{title:i.title,fixed:r}),e.jsx(a,{landingData:i,isPrev:t})]})}export{p as default};
