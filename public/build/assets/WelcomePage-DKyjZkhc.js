e28c77fd8e16c2b646c68f772128bf1450b182ce
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2025-02-07 15:20:59 -0600

    build genially

---
+++ public/build/assets/WelcomePage-DKyjZkhc.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-BQMXWDVi.js";import{N as s,P as a}from"./PrincipalVideo-ChgwCHjP.js";import"./proxy-CbzE_EaI.js";function p({landingData:i,fixedNav:r=!0,isPrev:t}){return e.jsxs("div",{className:"landing-page",children:[e.jsx(s,{title:i.title,fixed:r}),e.jsx(a,{landingData:i,isPrev:t})]})}export{p as default};
