7e848ee26a54b420dba8b5fbbfd8840122f12f11
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2025-02-27 09:36:16 -0600

    cambio a 100mb

--- app/Http/Requests/ResourceRequest.php
+++ app/Http/Requests/ResourceRequest.php
@@ -27,7 +27,7 @@ class ResourceRequest extends FormRequest
             'resources.*.id' => 'nullable|exists:resources,id', // Si se proporciona, el ID debe existir
             'resources.*.title' => 'required_with:resources|string|max:255', // Solo es requerido si hay resources
             'resources.*.type' => 'required_with:resources|in:pdf,video,zip', // Agrega 'zip' como tipo permitido
-            'resources.*.file' => 'nullable|file|mimes:pdf,mp4,zip|max:20480',
+            'resources.*.file' => 'nullable|file|mimes:pdf,mp4,zip|max:102400',
         ];
     }
 }
